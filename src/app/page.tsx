2829fbb6a501a44adcfd26a29c14595b8cb707e8
Martin-Die <martin.die.7559@gmail.com>
2024-12-16 11:22:58 +0100

    head déplacé dans le layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
 import Moon from "./demo/moon";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Mon Aperçu Personnalisé</title>
-        <meta property="og:title" content="Titre de votre projet" />
-        <meta
-          property="og:description"
-          content="Description qui apparaît avec votre aperçu."
-        />
-        <meta property="og:url" content="https://votre-site.com" />
-        <meta
-          property="og:image"
-          content="https://votre-site.com/chemin-vers-image.png"
-        />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <Moon />
     </div>
   );
