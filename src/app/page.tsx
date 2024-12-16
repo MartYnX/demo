6d827e6fbf45c325f57fe89b7f7bafad2830eef2
Martin-Die <martin.die.7559@gmail.com>
2024-12-16 15:11:17 +0100

    fix 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Bienvenue sur la page d'accueil</h1>
-            <Link href="/moon">
-                <button>Voir la Lune</button>
-            </Link>
+      <h1>Bienvenue sur la page d&apos;accueil</h1>
+      <Link href="/moon">
+        <button>Voir la Lune</button>
+      </Link>
     </div>
   );
 }
