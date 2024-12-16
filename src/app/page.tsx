6eee34a3b6e7ac0b0fdb329704ebb3bbfeba6d93
Martin-Die <martin.die.7559@gmail.com>
2024-12-16 11:18:58 +0100

    ajout de head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 import Moon from "./demo/moon";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Moon />
+    <div>
+      <Head>
+        <title>Mon Aperçu Personnalisé</title>
+        <meta property="og:title" content="Titre de votre projet" />
+        <meta
+          property="og:description"
+          content="Description qui apparaît avec votre aperçu."
+        />
+        <meta property="og:url" content="https://votre-site.com" />
+        <meta
+          property="og:image"
+          content="https://votre-site.com/chemin-vers-image.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Moon />
+    </div>
   );
 }
