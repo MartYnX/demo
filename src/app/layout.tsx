2012b7eb58462c2af357bf3aac9abe5b472708ca
Martin-Die <martin.die.7559@gmail.com>
2024-12-16 14:14:07 +0100

    test 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,38 @@ export const metadata: Metadata = {
     template: "%s | demo",
   },
   description: 'The moon',
+  openGraph: {
+    title: "Demo | Build and Ship Software",
+    description: "Join millions of developers building and shipping software on a collaborative platform.",
+    url: "https://monsiteweb.com",
+    siteName: "MonSiteWeb",
+    images: [
+      {
+        url: "/shadu.png", // Remplacez par l'URL de votre image
+        width: 1200,
+        height: 630,
+        alt: "Image de prévisualisation pour MonSiteWeb",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        media: '(prefers-color-scheme: light',
+        url: '/paw.svg',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        media: '(prefers-color-scheme: dark)',
+        url: '/paw.svg'
+      }
+    ]
+  },
 };
 
 export default function RootLayout({
