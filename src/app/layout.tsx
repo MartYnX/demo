88dc3d86258960d0d0cb9e140c35dc0dbc35325f
Martin-Die <martin.die.7559@gmail.com>
2024-12-16 11:28:04 +0100

    test 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,29 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Moon",
-  description: "The moon",
+  title: "Demo",
   openGraph: {
-    title: "Moon - A Beautiful Journey",
-    description: "Explore the beauty and mysteries of the moon.",
-    url: "https://votre-site.com",
-    siteName: "Moon Project",
-    images: [
-      {
-        url: "https://votre-site.com/moon-image.png", // Remplacez par l'URL de votre image
-        width: 1200,
-        height: 630,
-        alt: "Moon illustration",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Moon - A Beautiful Journey",
-    description: "Explore the beauty and mysteries of the moon.",
-    images: ["https://votre-site.com/moon-image.png"], // URL de l'image
+    title: 'Moon',
+    description: 'The moon',
   },
 };
 
