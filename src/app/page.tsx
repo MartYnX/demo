ca035a93119d78a3e3fbace227c4af67d6215b00
Martin-Die <martin.die.7559@gmail.com>
2024-12-18 17:19:55 +0100

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,7 @@ export default function Home() {
       <Link href="/gol">
         <button>game of life</button>
       </Link>
-      <Link href="/fmng">
-        <button>FursuitMakerNameGenerator</button>
-      </Link>
+
       <Link href="/glitch">
         <button>text</button>
       </Link>
