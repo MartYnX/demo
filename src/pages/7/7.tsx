5476c7fb4e0128567f623723dbd3722254577f3c
Martin-Die <martin.die.7559@gmail.com>
2024-12-22 10:07:35 +0100

    7 fix

--- src/pages/7/7.tsx
+++ src/pages/7/7.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import './7.css';
-import react, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 const Seven: React.FC = () => {
