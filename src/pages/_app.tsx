6ab5d24b8c4e88058e8fe7a7d7263f77f2d6e48d
Martin-Die <martin.die.7559@gmail.com>
2024-12-22 09:55:45 +0100

    konami fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
+import KonamiCode from '@/libs/konami';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
+            <KonamiCode />
         </>
     );
 }
